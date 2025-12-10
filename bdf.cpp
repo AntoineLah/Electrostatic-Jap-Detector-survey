5853236207e6478f0a257f8fef487b8b43655841
AntoineLah <alahaie@free.fr>
2025-12-10 12:00:23 +0100

    Remove redundant comments in bdf.cpp

--- bdf.cpp
+++ bdf.cpp
@@ -121,7 +121,7 @@ std::map<std::string, ROIResult> integrateROIs(const std::vector<double>& spectr
     return results;
 }
 
-// --- Binned TGraphErrors including statistical + systematic ---
+// --- Binned TGraphErrors including statistical ---
 TGraphErrors* makeBinnedGraph(const std::vector<double>& times,
                               const std::vector<double>& values,
                               int hoursBin,
@@ -172,7 +172,6 @@ TGraphErrors* makeBinnedGraph(const std::vector<double>& times,
     return g;
 }
 
-// --- Main processing ---
 void bdf() {
     struct DatasetConfig {
         std::string path;
@@ -317,7 +316,7 @@ void bdf() {
 
 
 
-        // --- Binned graph with total errors ---
+        // --- Binned graph with errors ---
         long long tMin = static_cast<long long>(*std::min_element(all_timestamp.begin(),all_timestamp.end()));
         long long tMax = static_cast<long long>(*std::max_element(all_timestamp.begin(),all_timestamp.end()));
         int hoursBin = 3;
@@ -368,8 +367,6 @@ void bdf() {
 
         // TLine ! 
         double f1, f2, f3, f4;
-        
-        
 
 
     } else std::cout<<"No points to plot\n";
